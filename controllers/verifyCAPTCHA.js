c3336bf14e18609fb5e70baa2efbdd8a42ee1b63
Mahfuzur Rahman <51076032+Mahfuzz11@users.noreply.github.com>
2024-04-30 12:33:10 +0600

    updating request of captcha

--- controllers/verifyCAPTCHA.js
+++ controllers/verifyCAPTCHA.js
@@ -1,11 +1,12 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-
 const verifyCAPTCHA = (req, res) => {
     const {captchaValue} = req.body;
 
-    fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SITE_SECRETE}&response=${captchaValue}`).then((result) => {
+    fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SITE_SECRETE}&response=${captchaValue}`, {
+        method: 'POST',
+    }).then((result) => {
         result.json().then((data) => {
             res.json({
                 success: true,
